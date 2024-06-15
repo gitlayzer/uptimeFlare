ad660c5840e9fc59c0a01db038e271f812f7a7f7
Gitlayzer <18037803502@163.com>
2024-06-15 14:30:13 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,10 +21,6 @@ export default function Header() {
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
         <div>
-          <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">
-            <Text size="xl" span>
-              🕒
-            </Text>
             <Text
               size="xl"
               span
@@ -32,7 +28,7 @@ export default function Header() {
               variant="gradient"
               gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
             >
-              UptimeFlare
+              Monitor
             </Text>
           </a>
         </div>
