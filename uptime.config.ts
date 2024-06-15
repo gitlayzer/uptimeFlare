0e0a2c3b5415c795043b7a312834cd2d65fade19
Gitlayzer <18037803502@163.com>
2024-06-15 14:22:29 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -17,44 +17,65 @@ const workerConfig = {
     // Example HTTP Monitor
     {
       // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'foo_monitor',
+      id: 'blog',
       // `name` is used at status page and callback message
-      name: 'My API Monitor',
+      name: 'Gitlayze`s Blog',
       // `method` should be a valid HTTP Method
-      method: 'POST',
+      method: 'GET',
       // `target` is a valid URL
-      target: 'https://example.com',
+      target: 'https://blog.devops-engineer.com.cn',
       // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
-      tooltip: 'This is a tooltip for this monitor',
+      tooltip: 'Gitlayze`s Blog',
       // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      statusPageLink: 'https://example.com',
+      statusPageLink: 'https://blog.devops-engineer.com.cn',
       // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
       expectedCodes: [200],
       // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
       timeout: 10000,
-      // [OPTIONAL] headers to be sent
-      headers: {
-        'User-Agent': 'Uptimeflare',
-        Authorization: 'Bearer YOUR_TOKEN_HERE',
-      },
-      // [OPTIONAL] body to be sent
-      body: 'Hello, world!',
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      responseKeyword: 'success',
-      // [OPTIONAL] if specified, the check will run in your specified region,
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-      checkLocationWorkerRoute: 'https://xxx.example.com',
     },
-    // Example TCP Monitor
     {
-      id: 'test_tcp_monitor',
-      name: 'Example TCP Monitor',
-      // `method` should be `TCP_PING` for tcp monitors
-      method: 'TCP_PING',
-      // `target` should be `host:port` for tcp monitors
-      target: '1.2.3.4:22',
-      tooltip: 'My production server SSH',
-      statusPageLink: 'https://example.com',
+      id: 'www',
+      name: 'Gitlayze`s Blog`s CNAME',
+      method: 'GET',
+      target: 'https://www.devops-engineer.com.cn',
+      tooltip: 'Gitlayze`s Blog',
+      statusPageLink: 'https://www.devops-engineer.com.cn',
+      timeout: 5000,
+    },
+    {
+      id: 'registry',
+      name: 'Image Acceleration',
+      method: 'GET',
+      target: 'https://registry.devops-engineer.com.cn',
+      tooltip: 'Image Acceleration',
+      statusPageLink: 'https://registry.devops-engineer.com.cn',
+      timeout: 5000,
+    },
+    {
+      id: 'picture',
+      name: 'Gitlayze`s Picture bed',
+      method: 'GET',
+      target: 'https://picture.devops-engineer.com.cn',
+      tooltip: 'Gitlayze`s Picture bed',
+      statusPageLink: 'https://picture.devops-engineer.com.cn',
+      timeout: 5000,
+    },
+    {
+      id: 'linux',
+      name: 'Gitlayze`s Linux Commands',
+      method: 'GET',
+      target: 'https://linux.devops-engineer.com.cn',
+      tooltip: 'Gitlayze`s Linux Commands',
+      statusPageLink: 'https://linux.devops-engineer.com.cn',
+      timeout: 5000,
+    },
+    {
+      id: 'dbdesign',
+      name: 'Gitlayze`s DBdesign',
+      method: 'GET',
+      target: 'https://dbdesign.devops-engineer.com.cn',
+      tooltip: 'Gitlayze`s DBdesign',
+      statusPageLink: 'https://dbdesign.devops-engineer.com.cn',
       timeout: 5000,
     },
   ],
